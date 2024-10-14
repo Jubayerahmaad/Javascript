fa322ed0249f6b23eb1197e2b71868dc8707fb18
Jubayerahmaad <jubuofficial84@gmail.com>
2024-10-15 01:48:52 +0600

    Day - 3 If else stetment usecase

---
+++ Day-3/js/app.js
@@ -0,0 +1,43 @@
+/* Todays topic if else stetment */
+
+
+// user age qual example
+
+let userAge = 18;
+
+if (userAge == 18) {
+    console.log('You Watch This Video');
+
+}else{
+    console.log('You Not Access This Videos');
+}
+
+// user age less the 18 example
+
+let userAges = 17;
+
+if (userAges < 18) {
+    console.log('You Watch This Video');
+
+}else{
+    console.log('You Not Access This Videos');
+}
+
+// User age not equel
+
+let userAgee = 18;
+
+if (userAgee != 18) {
+    console.log('You Watch This Video');
+
+}else{
+    console.log('You Not Access This Videos');
+}
+
+// Tirnery Oparetor oneline use if else stetment 
+
+let isUser = 16;
+
+let isUsers = isUser < 17 ? true : false;
+
+console.log(isUsers);
