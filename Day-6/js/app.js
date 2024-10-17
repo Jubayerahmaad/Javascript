6aa720938ef3463bbedc30fb600f498020f13a31
Jubayerahmaad <jubuofficial84@gmail.com>
2024-10-18 01:36:43 +0600

    math function

---
+++ Day-6/js/app.js
@@ -0,0 +1,19 @@
+function sum1(number1 , number2){
+    console.log(number1 + number2);
+}
+sum1(10, 20);
+
+function sum2(number1 , number2){
+    console.log(number1 - number2);
+}
+sum2(10, 20);
+
+function sum3(number1 , number2){
+    console.log(number1 * number2);
+}
+sum3(10, 20);
+
+function sum(number1 , number2){
+    console.log(number1 / number2);
+}
+sum(10, 20);
