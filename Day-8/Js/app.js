ac2f12840f5f58dedd2f5fac74d2c29ae04651b8
Jubayerahmaad <jubuofficial84@gmail.com>
2024-10-19 00:03:47 +0600

    Dom & Element Change Js

---
+++ Day-8/Js/app.js
@@ -0,0 +1,18 @@
+
+let pTag = document.getElementById('demo');
+
+// Change Text 
+pTag.innerHTML = 'Hello Dom!'
+
+// Add Class 
+pTag.classList.add('Dom');
+
+// add style 
+pTag.style.color ='red';
+pTag.style.padding = '10px 25px'
+pTag.style.width = '180px'
+
+// add style - 
+pTag.style.backgroundColor = 'green'
+pTag.style.fontSize = '25px'
+pTag.style.textAlign = 'center'
