0fc6191e15b6d531e02be73902f2eca747e578c1
Jubayerahmaad <jubuofficial84@gmail.com>
2024-10-29 18:06:25 +0600

    array methods

---
+++ Day - 16 Array Madthod/js/app.js
@@ -0,0 +1,16 @@
+let students = ['Helal', 'Jubayer', 'Habib', 'Abdullah'];
+
+// students.push('Siam'); Add list of array last item 
+// students.unshift('siam'); add list of array first time
+// students.shift(); delete list of arrys first item
+// students.pop(); delete list of arry last item
+// students.indexOf(); Take Out array possition 
+
+console.log(students);
+
+
+
+let deleteAnyItem = students.indexOf('Habib');
+
+students.splice(deleteAnyItem, 1);
+
