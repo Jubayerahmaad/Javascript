deaaf354c8f0df6596712f952d958cb4eb197229
Jubayerahmaad <jubuofficial84@gmail.com>
2024-11-07 00:25:28 +0600

    API Syntex

---
+++ Day - 21 Api/app.js
@@ -0,0 +1,17 @@
+// API Conections 
+
+let postApi = 'https://jsonplaceholder.typicode.com/posts';
+
+fetch(postApi)
+.then(function(respone){
+
+    // resolve the promise 
+    return respone.json();
+    
+
+})
+.then(function(posts){
+    posts.forEach(function(item){
+        console.log(item.title);
+    });
+});
